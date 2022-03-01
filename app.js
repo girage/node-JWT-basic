6ca99313291e25615806b7131c7ca46a58b65082
girage <45748977+girage@users.noreply.github.com>
2022-03-02 04:27:18 +0700

    set up base

--- app.js
+++ app.js
@@ -2,26 +2,29 @@ require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
 const app = express();
+const connectDB = require('./db/connect');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorMiddleware = require('./middleware/error-handler');
 
 PORT = process.env.PORT || 4000;
-
+URI = process.env.MONGO_URI;
 
 
 // Middleware
 app.use(express.static('./public'));
 app.use(express.json);
 
+const start = async () => {
+  try {
+    await connectDB(URI);
 
-app.get('/', (req,res) => {
-  res.send('<h1> Hello World </h>');
-})
+    app.listen(PORT, (req, res) => {
+      console.log(`listening to port ${PORT}..`);
+    })
+  } catch (error) {
+    console.log(error);
+  }
 
-const start = () => {
-  app.listen(PORT, (req, res) => {
-    console.log(`listening to port ${PORT}..`);
-  })
 }
 
 start();
